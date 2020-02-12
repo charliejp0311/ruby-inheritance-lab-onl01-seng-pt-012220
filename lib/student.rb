af64b820cfa1022ba25d940d3cfb07d7c8447939
Charles Pelton <charliejp0311@gmail.com>
2020-02-12 23:49:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,18 @@ require_relative "./user.rb"
 class Student < User
   attr_accessor :knowledge
   @@all = []
+
   def initialize
       @knowledge = []
+      save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    
   end
 
 end
